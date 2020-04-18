e185789df9932ec20244fa9c60aa994ec869aa3d
EC2 Default User <hojokaban@gmail.com>
2020-04-18 15:00:57 +0000

    Modify to preview mails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,10 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = '0161bd85579e4b778136a5b6a32cb2c4.vfs.cloud9.us-east-2.amazonaws.com' 
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
 
