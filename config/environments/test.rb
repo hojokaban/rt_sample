406843f9430808eb568559c91b87dc2a740ab78a
EC2 Default User <hojokaban@gmail.com>
2020-04-18 15:53:15 +0000

    Add mailer test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,6 +33,7 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'https://0161bd85579e4b778136a5b6a32cb2c4.vfs.cloud9.us-east-2.amazonaws.com' }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
